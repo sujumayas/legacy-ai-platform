6e03cbe2c8b79c264fc6923ed8f7858acd14f4ca
Esen Espinosa <sujumayas@gmail.com>
2025-02-03 19:56:03 -0500

    Add AuthProvider component

--- frontend/src/components/auth/AuthProvider.tsx
+++ frontend/src/components/auth/AuthProvider.tsx
@@ -2,8 +2,8 @@
 
 import { SessionProvider } from 'next-auth/react';
 
-export default function AuthProvider({
-  children,
+export function AuthProvider({
+  children
 }: {
   children: React.ReactNode;
 }) {
